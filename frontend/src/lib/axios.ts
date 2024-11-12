ac3dbf839a068385d6f97d3a424ac6908016f8cc
root <root@ip-172-31-33-91.ap-south-1.compute.internal>
2024-11-12 11:03:07 +0000

    update docker file

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://43.204.126.146:5000/api" // production me external IP address use kar rahe ho
+    : "http://backend:5000/api", // Docker Compose me backend service ka naam use kar rahe ho
 });
+
