e5b6e7c898bc8c0939bc4556d6af2a2d7fb66440
Saurav Garg <72258364+sauravgarg547@users.noreply.github.com>
2024-11-14 13:30:53 +0530

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
-    ? "http://13.203.63.158:5000/api" // production me external IP address use kar rahe ho
+    ? "http://43.204.126.146:5000/api" // production me external IP address use kar rahe ho
     : "http://backend:5000/api", // Docker Compose me backend service ka naam use kar rahe ho
 });
 
