e98d0d228fe45016752102dff7ffdc4a59720877
Saurav Garg <72258364+sauravgarg547@users.noreply.github.com>
2024-12-20 15:33:48 +0530

    Update deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -31,9 +31,9 @@ echo "Waiting for backend pod to be ready..."
 kubectl wait --for=condition=ready pod -l app=backend -n spotify --timeout=300s
 
 echo "Setting up port forwarding for frontend service on port 3000..."
-#kubectl port-forward service/frontend-service -n spotify 3000:3000 --address=0.0.0.0 &
+nohup kubectl port-forward service/frontend-service -n spotify 3000:3000 --address=0.0.0.0 &
 
 echo "Setting up port forwarding for backend service on port 5000..."
-#kubectl port-forward service/backend-service -n spotify 5000:5000 --address=0.0.0.0 &
+nohup kubectl port-forward service/backend-service -n spotify 5000:5000 --address=0.0.0.0 &
 
 echo "All resources have been successfully applied and port forwarding is active!"
